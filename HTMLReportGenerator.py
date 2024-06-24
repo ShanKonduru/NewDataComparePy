7a4c103cb4513108abab633439a307fe0f9f5901
ShanKonduru <ShanKonduru@gmail.com>
2024-06-24 14:30:46 -0600

    added Sort, Search and Filter Option to the Report

--- HTMLReportGenerator.py
+++ HTMLReportGenerator.py
@@ -1,7 +1,7 @@
 class HtmlReportGenerator:
     def generate_html_report(self, merged_df, stats, dataset_id):
         """Generates an HTML report based on the comparison results and statistics."""
-        # Define the background colors based on the _Merge column values
+        # Define the background colors based on the _merge column values
         bg_colors = {
             "Exists in Source and Target": "000000; background: green;",
             "Exists in Source Only": "000000; background: red;",
@@ -9,22 +9,22 @@ class HtmlReportGenerator:
         }
 
         # Generate the HTML for the detailed differences table with conditional formatting
-        detailed_diff_html = "<table>\n"
-        detailed_diff_html += "<tr>" + "".join(f"<th>{col}</th>" for col in merged_df.columns) + "</tr>\n"
+        detailed_diff_html = "<table id='detailed_diff_table'>\n"
+        detailed_diff_html += "<thead><tr>" + "".join(f"<th>{col}</th>" for col in merged_df.columns) + "</tr></thead>\n"
+        detailed_diff_html += "<tbody>\n"
         
         for _, row in merged_df.iterrows():
             merge_status = row['_merge']
-            print(merge_status)
             bg_color = bg_colors.get(merge_status, "")
-            print(bg_color)
             detailed_diff_html += f"<tr style='color: #{bg_color}'>" + "".join(f"<td>{val}</td>" for val in row) + "</tr>\n"
-
-        detailed_diff_html += "</table>"
+        
+        detailed_diff_html += "</tbody>\n</table>"
 
         html = f"""
         <html>
         <head>
             <title>Comparison Report - {dataset_id}</title>
+            <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
             <style>
                 body {{ font-family: Arial, sans-serif; }}
                 table {{ border-collapse: collapse; width: 100%; }}
@@ -33,6 +33,13 @@ class HtmlReportGenerator:
                 .header {{ font-size: 24px; margin-bottom: 20px; }}
                 .section {{ margin-top: 20px; }}
             </style>
+            <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+            <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
+            <script>
+                $(document).ready(function() {{
+                    $('#detailed_diff_table').DataTable();
+                }});
+            </script>
         </head>
         <body>
             <div class="header">Comparison Report - {dataset_id}</div>
